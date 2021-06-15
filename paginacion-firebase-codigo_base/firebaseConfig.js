df4b3bb3458858e867d90171d032f4427d85177e
gianinougo <gianinougo@gmail.com>
2021-06-15 12:24:19 +0200

    Creación de paginación en firebase

---
+++ paginacion-firebase-codigo_base/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDCTtCa8T4OxByTMxQwYk1wPn6hlYAUapE",
+	authDomain: "paginacion-62619.firebaseapp.com",
+	projectId: "paginacion-62619",
+	storageBucket: "paginacion-62619.appspot.com",
+	messagingSenderId: "319411081134",
+	appId: "1:319411081134:web:bd5eb53ccc66e97284423f"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
